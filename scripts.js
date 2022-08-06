596a5e94d6da1ee6dc513be907c500e458d90c30
Melissa.Mendoza <melsanz1011@gmail.com>
2022-08-05 23:27:48 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+ function listaRepositorio(repositorioUrl){
+     fetch(repositorioUrl)
+     .then(function (response){
+         return Response.json();
+        })
+     }
+     fetch("https://api.github.com/users/Melsanz/repos")
+     if(function(resposta){
+        return json();
+     });
+    'catch' (function(erro){
+     console.error(erro)   
+    });
+
+
+
